e54ac010609a6e9433c8f1725a63276f852d56e3
fabrironzi <58420177+fabrironzi@users.noreply.github.com>
2020-09-01 15:04:21 +0200

    Update gradient_reversal_example.py

--- gradient_reversal_example.py
+++ gradient_reversal_example.py
@@ -78,7 +78,7 @@ class DANN(nn.Module):
             class_outputs = self.class_classifier(features)
             return class_outputs
                 
-def myalexnet(pretrained=False, progress=True, **kwargs):
+def Myalexnet(pretrained=False, progress=True, **kwargs):
     r"""AlexNet model architecture from the
     `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
     Args:
