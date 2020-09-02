391707c3179fa5d70892808e914bc431d052386b
fabrironzi <58420177+fabrironzi@users.noreply.github.com>
2020-09-02 16:23:02 +0200

    Update gradient_reversal_example.py

--- gradient_reversal_example.py
+++ gradient_reversal_example.py
@@ -33,7 +33,7 @@ class ReverseLayerF(Function):
 class DANN(nn.Module):
     def __init__(self, num_category=7, test_or_train=2):
         super(DANN, self).__init__()
-        self.features = nn.Sequential(
+        self.feature_extractor = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
@@ -76,7 +76,7 @@ class DANN(nn.Module):
         self.domain_classifier[4].bias.data = copy.deepcopy(self.classifier[4].bias.data)
 
     def forward(self, x, alpha=None):
-        x = self.features(x)
+        x = self.feature_extractor(x)
         x = self.avgpool(x)
         features = torch.flatten(x, 1)
 
