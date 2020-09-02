d3ab68a98e44e8cef15f0aac4f998ba03a6da780
fabrironzi <58420177+fabrironzi@users.noreply.github.com>
2020-09-02 14:25:34 +0200

    Update gradient_reversal_example.py

--- gradient_reversal_example.py
+++ gradient_reversal_example.py
@@ -15,20 +15,22 @@ model_urls = {
     'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
 }
 
-class GradientReverse(Function):
+class ReverseLayerF(Function):
+    # Forwards identity
+    # Sends backward reversed gradients
     @staticmethod
     def forward(ctx, x, alpha):
         ctx.alpha = alpha
-        
+
         return x.view_as(x)
 
     @staticmethod
     def backward(ctx, grad_output):
-        out = grad_output.neg() * ctx.alpha
-        return out,None
+        output = grad_output.neg() * ctx.alpha
+
+        return output, None
 
 class DANN(nn.Module):
-
     def __init__(self, num_category=7, test_or_train=2):
         super(DANN, self).__init__()
         self.features = nn.Sequential(
@@ -46,9 +48,7 @@ class DANN(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
-        
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-
         self.classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
@@ -69,7 +69,7 @@ class DANN(nn.Module):
             nn.Linear(4096, test_or_train),
         )
 
-    def update_weigth(self):
+    def copy_weigth(self):
         self.domain_classifier[1].weight.data =  copy.deepcopy(self.classiﬁer[1].weight.data)
         self.domain_classifier[1].bias.data = copy.deepcopy(self.classiﬁer[1].bias.data)
         self.domain_classifier[4].weight.data = copy.deepcopy(self.classiﬁer[4].weight.data)
@@ -80,19 +80,15 @@ class DANN(nn.Module):
         x = self.avgpool(x)
         features = torch.flatten(x, 1)
 
-        classified = -1
-
         # If we pass alpha, we can assume we are training the discriminator
         if alpha is not None:
             # gradient reversal layer (backward gradients will be reversed)
-            reverse_feature = GradientReverse.apply(features,alpha)
-            domain_image = self.domain_classifier(reverse_feature)
-            classified = domain_image
+            reverse_feature = ReverseLayerF.apply(features,alpha)
+            discriminator_output = self.domain_classifier(reverse_feature)
+            return discriminator_output
         else:
-            category_image = self.classifier(features)
-            classified = category_image
-
-        return classified
+            class_outputs = self.classifier(features)
+            return class_outputs
 
 
 def AlexNetDANN(pretrained=False, progress=True, **kwargs):
@@ -112,6 +108,6 @@ def AlexNetDANN(pretrained=False, progress=True, **kwargs):
         state_dict.popitem("classifier.6.bias")
         state_dict.popitem("classifier.6.weight") 
         model.load_state_dict(state_dict,strict=False)
-        model.update_weigth()
+        model.copy_weigth()
 
     return model
