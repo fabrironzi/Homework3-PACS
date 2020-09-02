9041449a4489c51dc24047b4e6594409a919251d
fabrironzi <58420177+fabrironzi@users.noreply.github.com>
2020-09-02 12:42:53 +0200

    Update gradient_reversal_example.py

--- gradient_reversal_example.py
+++ gradient_reversal_example.py
@@ -2,17 +2,11 @@ import torch
 import torch.nn as nn
 import copy
 from torch.autograd import Function
-
 try:
     from torch.hub import load_state_dict_from_url
 except ImportError:
     from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
-''' 
-Very easy template to start for developing your AlexNet with DANN 
-Has not been tested, might contain incompatibilities with most recent versions of PyTorch (you should address this)
-However, the logic is consistent
-'''
 
 __all__ = ['AlexNet', 'alexnet']
 
@@ -21,27 +15,23 @@ model_urls = {
     'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
 }
 
-
-class ReverseLayerF(Function):
-    # Forwards identity
-    # Sends backward reversed gradients
+class GradientReverse(Function):
     @staticmethod
     def forward(ctx, x, alpha):
         ctx.alpha = alpha
-
+        
         return x.view_as(x)
 
     @staticmethod
     def backward(ctx, grad_output):
-        output = grad_output.neg() * ctx.alpha
-
-        return output, None
-
+        out = grad_output.neg() * ctx.alpha
+        return out,None
 
 class DANN(nn.Module):
+
     def __init__(self, num_category=7, test_or_train=2):
         super(DANN, self).__init__()
-        self.feature_extractor = nn.Sequential(
+        self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
@@ -56,8 +46,10 @@ class DANN(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
+        
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.class_classifier = nn.Sequential(
+
+        self.classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
@@ -66,6 +58,7 @@ class DANN(nn.Module):
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_category),
         )
+
         self.domain_classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
@@ -75,39 +68,37 @@ class DANN(nn.Module):
             nn.ReLU(inplace=True),
             nn.Linear(4096, test_or_train),
         )
-        
-    def copy_weight(self):
-        self.domain_classifier[1].weight.data =  copy.deepcopy(self.class_classiﬁer[1].weight.data)
-        self.domain_classifier[1].bias.data = copy.deepcopy(self.class_classiﬁer[1].bias.data)
-        self.domain_classifier[4].weight.data = copy.deepcopy(self.class_classiﬁer[4].weight.data)
-        self.domain_classifier[4].bias.data = copy.deepcopy(self.class_classiﬁer[4].bias.data)
+
+    def update_weigth(self):
+        self.domain_classifier[1].weight.data =  copy.deepcopy(self.classiﬁer[1].weight.data)
+        self.domain_classifier[1].bias.data = copy.deepcopy(self.classiﬁer[1].bias.data)
+        self.domain_classifier[4].weight.data = copy.deepcopy(self.classiﬁer[4].weight.data)
+        self.domain_classifier[4].bias.data = copy.deepcopy(self.classiﬁer[4].bias.data)
 
     def forward(self, x, alpha=None):
-        # feature_extractor = self.feature_extractor(x)
-        # feature_extractor = self.avgpool(x)
-        
-        x = self.feature_extractor(x)
+        x = self.features(x)
         x = self.avgpool(x)
-        feature_extractor = torch.flatten(x, 1)
-        
-        # Flatten the features:
-        # feature_extractor = feature_extractor.view(-1, self.cnn_num_features)
-        #feature_extractor = feature_extractor.view(feature_extractor.size(0), -1)
+        features = torch.flatten(x, 1)
+
+        classified = -1
+
         # If we pass alpha, we can assume we are training the discriminator
         if alpha is not None:
             # gradient reversal layer (backward gradients will be reversed)
-            reverse_feature = ReverseLayerF.apply(feature_extractor, alpha)
-            discriminator_output = self.domain_classifier(reverse_feature)
-            return discriminator_output
-        # If we don't pass alpha, we assume we are training with supervision
+            reverse_feature = GradientReverse.apply(features,alpha)
+            domain_image = self.domain_classifier(reverse_feature)
+            classified = domain_image
         else:
-            # do something else
-            class_outputs = self.class_classifier(feature_extractor)
-            return class_outputs
-                
-def Myalexnet(pretrained=False, progress=True, **kwargs):
+            category_image = self.classifier(features)
+            classified = category_image
+
+        return classified
+
+
+def AlexNetDANN(pretrained=False, progress=True, **kwargs):
     r"""AlexNet model architecture from the
     `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
@@ -116,9 +107,11 @@ def Myalexnet(pretrained=False, progress=True, **kwargs):
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls['alexnet'],
                                               progress=progress)
+        
+        # removing unused params
         state_dict.popitem("classifier.6.bias")
         state_dict.popitem("classifier.6.weight") 
-        model.load_state_dict(state_dict, strict=False)
-        model.copy_weight()
-        
+        model.load_state_dict(state_dict,strict=False)
+        model.update_weigth()
+
     return model
